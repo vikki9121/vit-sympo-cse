d60869d6795be7d0cda49e4302cc84e862d06391
rajeshrcse <r21161500@gmail.com>
2023-02-11 21:31:46 +0530

    new commit

--- js/auto.js
+++ js/auto.js
@@ -3,17 +3,7 @@ $(window).scroll(function(){
     document.getElementById("registerHeroButton").style.background=`linear-gradient(${$(window).scrollTop()%360+45}deg,var(--neonBlue), var(--neonPink), var(--neonViolet))`;
 });
 
-$(document).ready(function() {
-    setTimeout(function() {  myScrollTo('aboutdiv') }, 5000);
-});
-
-function myScrollTo(id="aboutdiv"){
-    document.getElementById(id).scrollIntoView({behavior:"smooth",block:"center"});
-}
-
 function openInNewTab(url){
     window.open(url, '_blank').focus();
 }
 
-new FlipDown(1567390400, "registerBy",{theme:"light"}).start();
-new FlipDown(1567619000, "eventBy",{theme:"light"}).start();
--- js/page.js
+++ js/page.js
@@ -3,14 +3,6 @@ $(window).scroll(function(){
     document.getElementById("registerHeroButton").style.background=`linear-gradient(${$(window).scrollTop()%360+45}deg,var(--neonBlue), var(--neonPink), var(--neonViolet))`;
 });
 
-$(document).ready(function() {
-    setTimeout(function() {  myScrollTo('aboutdiv') }, 5000);
-});
-
-function myScrollTo(id="aboutdiv"){
-    document.getElementById(id).scrollIntoView({behavior:"smooth",block:"center"});
-}
-
 function openInNewTab(url){
     window.open(url, '_blank').focus();
 }
